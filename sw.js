496e9941b43ce01fad068a6671d2edd96147b6e5
chintanaaiv-coder <chintana.aiv@gmail.com>
2026-05-29 13:40:43 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aga-tasks-v50';
+const CACHE_NAME = 'aga-tasks-v51';
 
 // Import Firebase Messaging SW
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
